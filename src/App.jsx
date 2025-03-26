8ebceda69f4814f6ad59517935b81bb184b57676
20bec4199 <manitamilselvanvishnu007@gmail.com>
2025-03-26 23:19:35 +0530

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,16 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Demo from './components/Demo'
+import './index.css';
+import { LoadingScreen } from './components/sections/LoadingScreen'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [isLoaded,setIsLoaded] = useState(false);
 
   return (
     <>
-     <Demo />
+   {!isLoaded && <LoadingScreen onComplete={ () => setIsLoaded(true)} />}
     </>
   )
 }
