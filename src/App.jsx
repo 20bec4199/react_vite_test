d8d66a44f93ece852984b045da1f3fa7f0f9d97d
20bec4199 <manitamilselvanvishnu007@gmail.com>
2025-03-26 23:54:28 +0530

    fourth commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,21 @@ import './App.css'
 import Demo from './components/Demo'
 import './index.css';
 import { LoadingScreen } from './components/sections/LoadingScreen'
+import { Navbar } from './components/sections/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
   const [isLoaded,setIsLoaded] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
 
   return (
     <>
    {!isLoaded && <LoadingScreen onComplete={ () => setIsLoaded(true)} />}
+    <div className={`min-h-screen transition-opacity duration-700 ${
+      isLoaded ? 'opacity-100' : 'opacity-0'
+    } bg-black text-gray-100`}>
+    <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+    </div>
     </>
   )
 }
