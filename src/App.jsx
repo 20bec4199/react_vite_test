4013fd74ebe49d9421da9f5d862caab8f60da1d6
20bec4199 <manitamilselvanvishnu007@gmail.com>
2025-04-21 15:01:18 +0530

    Portfolio Completed

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import { MobileMenu } from './components/sections/MobileMenu'
 import { Home } from './components/sections/Home'
 import { About } from './components/sections/About'
 import { Projects } from './components/sections/Project'
+import { Contact } from './components/sections/Contact'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -27,6 +28,7 @@ function App() {
     <Home />
     <About />
     <Projects />
+    <Contact />
     </div>
     {/* <Demo /> */}
     </>
